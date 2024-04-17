9c283f609ef516e67fa76354bffb6fe903ef29d4
Bazil <bazil4u@gmail.com>
2024-04-16 23:18:25 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
 from setuptools import setup, find_packages
+import os
+
+# Optional: Load long description from README.md
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='easypy',
     version='0.1.0',
+    description='A python package helps in repeating your print statements and calling classes as much as you needed with *',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/BazilNizam/easyclass',
+    author='Bazil Nizam',
+    author_email='bazil_nizam@yhoo.com',
+    license='MIT',
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
     entry_points={
         'console_scripts': [
             'easypy=easypy.interpreter:main',
         ],
     },
-    license='MIT',
-    author='Bazil Nizam',
-    author_email='bazil_nizam@yhoo.com',
-    description='A python package helps in repeating your print statements and calling classes as much as u needede with * ',
-    keywords='easypy',
-    url='https://github.com/BazilNizam/easyclass',  # Optional
 )
